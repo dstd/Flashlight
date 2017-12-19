d95de9fa5fd47c826ccc10e196ef9b9cb78b0403
Denis Stanishevskiy <std.denis@gmail.com>
2017-12-20 01:04:17 +0300

    New syntax of query: `gtr ~text(this is a text) to ~lang(cz)`

--- PluginDirectories/1/googletranslate.bundle/plugin.py
+++ PluginDirectories/1/googletranslate.bundle/plugin.py
@@ -1,38 +1,49 @@
+# coding=UTF-8
 import urllib
 import json
+import os
 
+def fix_lang_shortcut(lang):
+    return {'cn':'zh-CN', 'tw':'zh-TW'}.get(lang, lang)
 
 def results(parsed, original_query):
     url = 'https://translate.google.com/m/translate#<LANG_FROM>/<LANG_TO>/<TEXT>'
 
-    lang_from = original_query[4:6].encode('UTF-8')
-    lang_to = original_query[6:8].encode('UTF-8')
+    if parsed.get('~lang') == None:
+        lang_from = original_query[4:6].encode('UTF-8')
+        lang_to = original_query[6:8].encode('UTF-8')
+        if original_query[8:9] != ' ':
+            return
+        parsed['~lang'] = "%s%s" % (lang_from, lang_to)
 
-    text = parsed['~text'].encode('UTF-8')
-    if text[0:4] == '%s%s' % (lang_from, lang_to):
-        text = text[5:]
+    text = parsed.get('~text', '').encode('UTF-8')
+    lang = parsed.get('~lang', '').encode('UTF-8').split('-')
+    
+    if len(lang) == 4:
+        from_lang = fix_lang_shortcut(lang[0:2])
+        to_lang = fix_lang_shortcut(lang[2:4])
+        title = "Translate %s → %s" % (from_lang, to_lang)
+    elif len(lang) == 2:
+        from_lang = 'auto'
+        to_lang = fix_lang_shortcut(lang)
+        title = "Translate to %s" % (lang)
+    elif
+        return
 
-    # Shortcuts for non-2-characters language codes
-    lang_mapping = { 'cn':'zh-CN', 'tw':'zh-TW' }
-    for k, v in lang_mapping.iteritems():
-        lang_from = lang_from.replace(k, v)
-        lang_to = lang_to.replace(k, v)
-
-    url = url.replace('<LANG_FROM>', lang_from)
-    url = url.replace('<LANG_TO>', lang_to)
+    url = url.replace('<LANG_FROM>', from_lang)
+    url = url.replace('<LANG_TO>', to_lang)
     url = url.replace('<TEXT>', urllib.quote_plus(text))
 
     return {
-        "title": "Translate '%s' from %s to %s" % (text, lang_from, lang_to),
+        "title": title,
         "run_args": [url],
-        "html": "<script>window.location=%s</script>" % json.dumps(url),
+        "html": "<script>setTimeout(function() { window.location = %s }, 500)</script>" % json.dumps(url),
         "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
         "webview_links_open_in_browser": True
     }
 
 
 def run(url):
-    import os
     os.system('open "{0}"'.format(url.replace('/m/translate', '/')))
 
 # EOF
